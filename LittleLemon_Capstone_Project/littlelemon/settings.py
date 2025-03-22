ac120c9e0d6934fbea6d72b847386392dfee84d2
Precious <banigoprecious@yahoo.com>
2025-03-22 13:57:26 +0100

    Back-End Developer Capstone

--- LittleLemon_Capstone_Project/littlelemon/settings.py
+++ LittleLemon_Capstone_Project/littlelemon/settings.py
@@ -78,12 +78,12 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': 'reservations',
-        # 'HOST' : '127.0.0.1',
-        # 'PORT' : '3306',
-        # 'USER' : 'admindjango',
-        # 'PASSWORD' : 'employee@123!',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'reservations',
+        'HOST' : '127.0.0.1',
+        'PORT' : '3306',
+        'USER' : 'admindjango',
+        'PASSWORD' : 'employee@123!',
     }
 }
 
